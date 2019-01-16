8a855824a58fa89fc992a6940ec2bfcc87f64fbf
Keegan Hanes <keeganhanes4@gmail.com>
2019-01-15 20:10:32 -0800

    Pass all tests

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,6 +10,44 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        var result: Int = 0
+        if args.last == "count" {
+            result = args.count - 1
+        } else if args.last == "avg" {
+            if args.count - 1 != 0 {
+                for index in 1...(args.count - 1) {
+                    result += Int(args[index - 1])!
+                }
+                result = result / (args.count - 1)
+            }
+        } else if args.last == "fact" {
+            if args.count - 1 != 0 {
+                result = 1
+                let n: Int = Int(args[0])! + 1
+                for i in 1..<n {
+                    result = result * i
+                }
+            }
+        } else {
+            let first = Int(args[0])
+            let operation = args[1]
+            let second = Int(args[2])
+            switch operation {
+            case "+":
+                result = first! + second!
+            case "-":
+                result = first! - second!
+            case "*":
+                result = first! * second!
+            case "/":
+                result = first! / second!
+            case "%":
+                result = first! % second!
+            default:
+                result = 0
+            }
+        }
+        return result
     }
     
     public func calculate(_ arg: String) -> Int {
